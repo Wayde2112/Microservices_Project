0612dda8874b68c93f630313853c32899b25bbd9
Stefano Saldana <saldana.stefano@gmail.com>
2023-01-17 12:57:47 +0100

    deleting "node-prova"

--- node-prova/src/config.js
+++
@@ -1,6 +0,0 @@
-import {config} from 'dotenv'
-config()
-
-export default {
-  port: process.env.PORT || 3000
-}
--- node-prova/src/index.js
+++
@@ -1,5 +0,0 @@
-import app from './app'
-
-app.listen(app.get('port'))
-
-console.log('Server listening on port', app.get('port'))
