75bc35293b29ac2aab23e506b151e7e5660882fa
Stefano Saldana <saldana.stefano@gmail.com>
2023-01-20 09:56:21 +0100

    Update querys.js

--- books-service/src/database/querys.js
+++ books-service/src/database/querys.js
@@ -1,5 +1,5 @@
 export const queries = {
-  createTable: CREATE TABLE "book" ( bookId BIGINT, bookName character varying, bookAuthor character varying ) TABLESPACE pg_default,
+  createTable: 'CREATE TABLE "book" ( bookId BIGINT, bookName character varying, bookAuthor character varying ) TABLESPACE pg_default',
   getAllBooks: 'SELECT * FROM book',
   getBookById: 'SELECT * FROM book WHERE id = @id',
   createNewBook: 'INSERT INTO book (Name, Author) VALUES (@Name, @Author)',
